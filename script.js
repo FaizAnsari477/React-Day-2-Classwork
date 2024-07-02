05a83d250c317c28808d5dc38d014ea733624487
FaizAnsari477 <faizansari477@gmail.com>
2024-07-02 16:45:13 +0530

    Adding Day 2 Assignment

---
+++ script.js
@@ -0,0 +1,34 @@
+const ReactAppUsingCDN = () => {
+  return (
+    <div>
+      <h1>Learn Web Development</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus
+        quaerat ut impedit nesciunt consequuntur obcaecati eius hic eligendi!
+        Voluptas, ea.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis
+        hic repellat obcaecati suscipit aliquam porro dolorum magnam, officia
+        maiores ullam, harum doloremque temporibus quo. Harum, illo. Amet
+        ratione illum sint rerum, <a href="https://react.dev/">Learn React</a>
+        doloribus, neque quia consectetur dolor omnis expedita porro qui iure
+        quisquam laudantium minima temporibus incidunt ad voluptatum et
+        delectus!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio
+        recusandae reiciendis officia debitis sunt aspernatur suscipit, quae
+        rerum tenetur iusto veritatis eum qui excepturi cupiditate quam nostrum
+        harum quos quas nobis modi placeat vel expedita nisi. Vel dolor
+        molestias molestiae inventore, animi repudiandae, sunt officiis quod
+        delectus iste explicabo vero, eius expedita! Magni necessitatibus
+        eligendi maiores quo, aspernatur iste architecto, nisi sed perferendis
+        suscipit, distinctio odio amet! Temporibus, possimus officiis maiores
+        nobis nulla delectus atque optio cum blanditiis quibusdam voluptatum.
+      </p>
+    </div>
+  );
+};
+
+ReactDOM.render(<ReactAppUsingCDN />, document.querySelector("#root"));
